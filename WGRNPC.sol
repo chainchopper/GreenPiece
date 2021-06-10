9950f494219f60000c3d687b4371c6cafd8be006
Justin Adams <36168576+chainchopper@users.noreply.github.com>
2021-06-10 06:58:22 -0400

    Add files via upload

---
+++ WGRNPC.sol
@@ -0,0 +1,82 @@
+pragma solidity ^0.5.8;
+
+import "./ITokenDeposit.sol";
+
+contract WGRNPC is ITokenDeposit {
+    string public name = "Wrapped Elon Green";
+    string public symbol = "WGRNPC";
+    uint8  public decimals = 6;
+    trcToken  public grnpcTokenId = trcToken(1004051);
+
+    uint256 private totalSupply_;
+    mapping(address => uint256) private  balanceOf_;
+    mapping(address => mapping(address => uint)) private  allowance_;
+
+
+    function() external payable {
+        deposit();
+    }
+
+    function deposit() public payable {
+        require(msg.tokenid == grnpcTokenId, "deposit tokenId not GRNPC[1004051]");
+        // tokenvalue is long value
+        balanceOf_[msg.sender] += msg.tokenvalue;
+        totalSupply_ += msg.tokenvalue;
+        emit Transfer(address(0x00), msg.sender, msg.tokenvalue);
+        emit Deposit(msg.sender, msg.tokenvalue);
+    }
+
+    function withdraw(uint256 sad) public {
+        require(balanceOf_[msg.sender] >= sad, "not enough WGRNPC balance");
+        require(totalSupply_ >= sad, "not enough WGRNPC totalSupply");
+        balanceOf_[msg.sender] -= sad;
+        totalSupply_ -= sad;
+        msg.sender.transferToken(sad, grnpcTokenId);
+
+        emit Transfer(msg.sender, address(0x00), sad);
+        emit Withdrawal(msg.sender, sad);
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return totalSupply_;
+    }
+
+    function balanceOf(address guy) public view returns (uint256){
+        return balanceOf_[guy];
+    }
+
+    function allowance(address src, address guy) public view returns (uint256){
+        return allowance_[src][guy];
+    }
+
+    function approve(address guy, uint256 sad) public returns (bool) {
+        allowance_[msg.sender][guy] = sad;
+        emit Approval(msg.sender, guy, sad);
+        return true;
+    }
+
+    function approve(address guy) public returns (bool) {
+        return approve(guy, uint256(- 1));
+    }
+
+    function transfer(address dst, uint256 sad) public returns (bool) {
+        return transferFrom(msg.sender, dst, sad);
+    }
+
+    function transferFrom(address src, address dst, uint256 sad)
+    public returns (bool)
+    {
+        require(balanceOf_[src] >= sad, "src balance not enough");
+
+        if (src != msg.sender && allowance_[src][msg.sender] != uint256(- 1)) {
+            require(allowance_[src][msg.sender] >= sad, "src allowance is not enough");
+            allowance_[src][msg.sender] -= sad;
+        }
+        balanceOf_[src] -= sad;
+        balanceOf_[dst] += sad;
+
+        emit Transfer(src, dst, sad);
+        return true;
+    }
+
+}
